7aaf22da57781c91e80383140397af06a502a8ee
chris <davischris25139@yahoo.com>
2023-11-19 23:06:44 -0800

    added updates inquirer prompts, added html generation and folder to drop new docs into, added data.badge to object to recall later for badges, updated markdown for html, made table of contents static instead of user choice, added snap function to table of contents.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,98 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to render the license badge
+function renderLicenseBadge(data) {
+  if (data) {
+    // console.log(data.badge)
+    // Return the license badge based on the license value
+    return data.badge;
+    
+  }
+  // Return an empty string if there is no license
+  return '';
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to render the license link
+function renderLicenseLink(license) {
+  if (license) {
+    // Return the license link based on the license value
+    return `[License](#license)`;
+  }
+  // Return an empty string if there is no license
+  return '';
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to render the license section of the README
+function renderLicenseSection(license) {
+  if (license) {
+    // Return the license section with the license value
+    return `## License
+
+This project is licensed under the ${license} license.`;
+  }
+  // Return an empty string if there is no license
+  return '';
+}
+
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const licenseBadge = renderLicenseBadge(data);
+  const licenseSection = renderLicenseSection(data.license);
 
+  const markdownContent = 
+  `
+# ${data.title}
+
+<br>
+
+${licenseBadge}
+
+## Table of Contents
++ [Desciption](#description)
++ [Installation](#installation)
++ [Usage](#usage)
++ [License](#license)
++ [Contributors](#contributors)
++ [Tests](#tests)
++ [Questions](#questions)
+
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+
+${licenseSection}
+
+
+## Contributors
+${data.contributors}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please contact me: 
+
+<br>
+
+[My Github](https://github.com/${data.github}?tab=repositories)
+
+<br>
+
+[Email Me](mailto:${data.email})
 `;
+
+  // Return the markdown content as a string
+  return markdownContent;
 }
 
-module.exports = generateMarkdown;
+
+// renderLicenseBadge;
+module.exports = generateMarkdown
--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
+
+const licenseChoices = [
+  { name: 'No License', value: 'none' },
+  { name: 'MIT License', value: 'MIT', badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'},
+  { name: 'Apache License 2.0', value: 'Apache-2.0', 
+      badge: "[![License: Apache-2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)" },
+  { name: 'GNU GPLv3', value: 'GPL-3.0',
+      badge: "[![License: GPL-3.0](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)" },
+  { name: 'BSD 3-Clause License', value: 'BSD-3-Clause',
+      badge: "[![License: BSD-3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)" },
+  { name: 'Mozilla Public License 2.0', value: 'MPL-2.0',
+      badge: "[![License: MPL-2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)" },
+  { name: 'Creative Commons Attribution 4.0 International', value: 'CC-BY-4.0',
+      badge: "[![License: CC-BY-4.0](https://licensebuttons.net/l/by/4.0/80x15.png)](https://creativecommons.org/licenses/by/4.0/)" },
+  { name: 'The Unlicense', value: 'Unlicense',
+      badge: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)" },
+  
+];
+
+
+
 inquirer
   .prompt([
     {
@@ -15,25 +37,62 @@ inquirer
       message: 'what is the description of your project?',
     },
     {
+      type: 'input',
+      name: 'installation',
+      message: 'Describe the steps to install your application',
+      
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the intended usage for your application?',
+      
+    },
+    {
       type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'telekinesis'],
+      name: 'license',
+      message: 'Choose a license for your application',
+      choices: licenseChoices,
+      transformer: (choice) => choice.value,
+      
+    },
+    {
+      type: 'input',
+      name: 'contributors',
+      message: 'List the names of your contributors.',
+      
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Explain the tests you have completed for your application.',
+      
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your github userName',
+      
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email',
+      
     },
   ])
   .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+    const selectedLicense = licenseChoices.find(
+      (choice) => choice.value === data.license);
+      data.badge = selectedLicense.badge;
+      
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
+    const filename = `./outputContent/${data.title.toLowerCase().split(' ').join('')}.md`;
+    const markdownContent = generateMarkdown(data);
+
+    fs.writeFile(filename, markdownContent, (err) =>
+    err ? console.log(err) : console.log('Success!')
     );
+    
+    generateMarkdown(data)
   });
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
